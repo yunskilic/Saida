70d8136e753452c12eb186b7be5573cd0eb43471
Yunus <yunskilic@gmail.com>
2017-09-09 13:17:37 +0300

    Refactoring

--- src/Tools/ToolBase.java
+++ src/Tools/ToolBase.java
@@ -15,9 +15,6 @@ import java.util.stream.Collectors;
 
 import static Utils.Constants.OutputFolder;
 
-/**
- * Created by YUNUS on 16.04.2017.
- */
 public abstract class ToolBase {
     private String inputPath;
 
@@ -32,24 +29,37 @@ public abstract class ToolBase {
                 System.out.println("There is not rule definition for this alert type");
                 continue;
             }
-            int fieldCount = StringUtils.countMatches(FileUtils.readFileToString(new File(mopPath), Charset.defaultCharset()), Constant.FIELDINFO);
-            List<ClassMember> fields = alert.getClassMemberList().stream().filter(x -> x.getClass().getName().equals(Field.class.getName())).collect(Collectors.toList());
+            int fieldCount = StringUtils.countMatches(FileUtils.readFileToString(new File(mopPath)
+                                                                                , Charset.defaultCharset())
+                                                                                , Constant.FIELDINFO);
+            List<ClassMember> fields = alert.getClassMemberList()
+                    .stream()
+                    .filter(x -> x.getClass().getName().equals(Field.class.getName()))
+                    .collect(Collectors.toList());
             if (fieldCount == fields.size()) {
                 for (int i = 0; i < fieldCount; i++) {
                     Field field = (Field)fields.get(i);
-                    String newStr = FileUtils.readFileToString(new File(mopPath), Charset.defaultCharset()).replaceFirst(Constant.FIELDINFO,String.format("%s %s.%s",field.getSignature(),field.getClassName(),field.getName()));
+                    String newStr = FileUtils.readFileToString(new File(mopPath)
+                                                            ,Charset.defaultCharset()).replaceFirst(Constant.FIELDINFO
+                                                            ,String.format("%s %s.%s",field.getSignature()
+                                                            ,field.getClassName()
+                                                            ,field.getName()));
                     FileUtils.writeStringToFile(new File(mopPath), newStr, Charset.defaultCharset());
                 }
             } else {
                 Field field = (Field)fields.get(0);
-                String newStr = FileUtils.readFileToString(new File(mopPath), Charset.defaultCharset()).replace(Constant.FIELDINFO, String.format("%s %s.%s",field.getSignature(),field.getClassName(),field.getName()));
+                String newStr = FileUtils.readFileToString(new File(mopPath), Charset.defaultCharset())
+                        .replace(Constant.FIELDINFO, String.format("%s %s.%s",field.getSignature(),field.getClassName(),field.getName()));
                 FileUtils.writeStringToFile(new File(mopPath), newStr, Charset.defaultCharset());
             }
         }
     }
 
     private String FindMopPath(String alertType) {
-        if (new File(String.format("%s\\%s.mop", OutputFolder, alertType)).exists())return String.format("%s\\%s.mop", OutputFolder, alertType);
+        String path = String.format("%s\\%s.mop", OutputFolder, alertType);
+        File file = new File(path);
+        if (file.exists())
+            return path;
         return StringUtils.EMPTY;
     }
 
